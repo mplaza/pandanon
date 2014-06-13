ecf361263c42a8210a6c5eb068b6ae79a0b8866b
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-06-12 19:33:00 -0700

    mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,9 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'anonie.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
