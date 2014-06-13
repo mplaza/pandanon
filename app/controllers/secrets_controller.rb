e9335f72b24839ae504772d7cc8476dd89cad0a1
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-06-13 14:31:13 -0700

    limit display

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,6 @@
 class SecretsController < ApplicationController
   def index
-  	@secrets = Secret.find_by_sql('SELECT * FROM secrets WHERE approved=true LIMIT 10').reverse
+  	@secrets = Secret.find_by_sql('SELECT * FROM secrets WHERE approved=true ORDER BY created_at LIMIT 10').reverse
   end
 
   def compindex
