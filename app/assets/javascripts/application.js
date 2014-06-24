28a74b209a23a8a0378ff367393ce0e506096116
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-06-23 21:25:05 -0700

    look im typing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,3 @@
 //= require_tree .
 
 
-function getGeoLocation() {
-  navigator.geolocation.getCurrentPosition(setGeoCookie);
-console.log('locating')
-}
-
-function setGeoCookie(position) {
-  var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
-  document.cookie = "lat_lng=" + escape(cookie_val);
-  console.log(cookie_val);
-}
-
